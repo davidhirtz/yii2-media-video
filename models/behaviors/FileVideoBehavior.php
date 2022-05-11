81eddf4bf34bdabf1f648c4789227a15da487fc5
davidhirtz <david.hirtz@me.com>
2022-05-11 11:13:21 +0200

    Update FileVideoBehavior.php

--- models/behaviors/FileVideoBehavior.php
+++ models/behaviors/FileVideoBehavior.php
@@ -5,7 +5,9 @@ namespace davidhirtz\yii2\media\video\models\behaviors;
 use davidhirtz\yii2\media\models\File;
 use Exception;
 use getID3;
+use Yii;
 use yii\base\Behavior;
+use yii\validators\FileValidator;
 
 /**
  * @property File $owner
@@ -18,11 +20,30 @@ class FileVideoBehavior extends Behavior
     public function events(): array
     {
         return [
+            File::EVENT_CREATE_VALIDATORS => 'onCreateValidators',
             File::EVENT_BEFORE_VALIDATE => 'onBeforeValidate',
         ];
     }
 
     /**
+     * Makes sure MP4 is added to the allowed extensions.
+     */
+    public function onCreateValidators()
+    {
+        if (!in_array('mp4', $this->owner->allowedExtensions)) {
+            $this->owner->allowedExtensions[] = 'mp4';
+
+            foreach ($this->owner->getValidators() as $validator) {
+                if ($validator instanceof FileValidator) {
+                    $validator->extensions = $this->owner->allowedExtensions;
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Sets `width` and `height` from video resolution.
      * @noinspection PhpUndefinedMethodInspection
      */
     public function onBeforeValidate()
