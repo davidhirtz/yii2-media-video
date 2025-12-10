b5f4eff1cc8360726db72824947ce4ea8337b7de
davidhirtz <david.hirtz@me.com>
2025-12-10 06:48:35 +0000

    Use same test db

--- tests/config/test.php
+++ tests/config/test.php
@@ -14,7 +14,7 @@ return [
     ],
     'components' => [
         'db' => [
-            'dsn' => getenv('MYSQL_DSN') ?: 'mysql:host=127.0.0.1;dbname=yii2_media_video_test',
+            'dsn' => getenv('MYSQL_DSN') ?: 'mysql:host=127.0.0.1;dbname=yii2_test',
             'username' => getenv('MYSQL_USER') ?: 'root',
             'password' => getenv('MYSQL_PASSWORD') ?: '',
             'charset' => 'utf8',
